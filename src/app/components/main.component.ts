658f809ffa1f481180fc5e4e95f35de9aea4605e
miriam_barrera <miriam.barrera@sanmina.com>
2022-07-20 16:54:09 -0500

    Firts commit

---
+++ src/app/components/main.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewChild } from '@angular/core';
+
+
+@Component({
+  selector: 'main-root',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+  title = 'Register100Ladrillos';
+ 
+}
